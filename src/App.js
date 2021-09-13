25e0bd0abfc26c1c62206dfe40045cf5606eaec1
shahriar <shahriarkabir050@gmail.com>
2021-09-13 22:23:43 +0600

    Categories, products , newsletter and footer created

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import React from 'react'
 import Announcement from './components/Announcement';
 import Navbar from './components/Navbar';
 import Slider from './components/Slider';
+import Categories from './components/Categories';
+import Products from './components/Products';
+import Newsletter from './components/Newsletter';
+import Footer from './components/Footer';
 
 
 const App = () => {
@@ -11,6 +15,11 @@ const App = () => {
           <Announcement/> 
           <Navbar/>
           <Slider/>
+          <Categories/>
+          <Products/>
+          <Newsletter/>
+          <Footer/>
+
 
     </div>
 
